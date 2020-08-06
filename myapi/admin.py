f711b22c3b9746b79c2f40ddf04f28c530a758ca
vikas <vikas.kumar2@oodlestechnologies.com>
2020-08-06 15:19:26 +0530

    'second_comit'

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
+from .forms import ProfileCreationForm, ProfileChangeForm
 from .models import Profile, Address
 # Register your models here.
-admin.site.register(Profile, UserAdmin)
+class ProfileUserAdmin(UserAdmin):
+    add_form = ProfileCreationForm
+    form = ProfileChangeForm
+    model= Profile
+    list_display = ('name','email','profile_pic','phone_number',)
+    list_filter = ('gender','address__city')
+
+    fieldsets = (
+        (None,{'fields':('email','password')}),
+        ('Permissions',{'fields':('is_staff','is_active')}),
+    )
+    add_fieldsets = (
+        (None,{
+            'classes':('wide',),
+            'fields':('email','password1','password2','is_staff','is_active')
+        }),
+    )
+    search_fields = ('name',)
+    ordering = ('name',)
+
 admin.site.register(Address)
+admin.site.register(Profile, ProfileUserAdmin)
